7a8d5cb9e2588d01f5d138a21b5ae4e245179df4
zhangwei <zhangwei@zhanyaa.com>
2018-06-07 14:44:40 +0800

    01

--- src/main/java/com/uc/netty/ConnectionWatchdog.java
+++ src/main/java/com/uc/netty/ConnectionWatchdog.java
@@ -1,13 +1,8 @@
 package com.uc.netty;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.*;
 import io.netty.channel.ChannelHandler.Sharable;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelInitializer;
 import io.netty.util.Timeout;
 import io.netty.util.Timer;
 import io.netty.util.TimerTask;
@@ -15,48 +10,47 @@ import io.netty.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 /**
- * 
  * 重连检测狗，当发现当前的链路不稳定关闭之后，进行12次重连
  */
 @Sharable
-public abstract class ConnectionWatchdog extends ChannelInboundHandlerAdapter implements TimerTask ,ChannelHandlerHolder{
-    
-    
-    
-    private final Bootstrap bootstrap;
+public abstract class ConnectionWatchdog extends ChannelInboundHandlerAdapter
+        implements TimerTask, ChannelHandlerHolder {
+
+    private final Bootstrap bootstrap;//重连对象的管理
     private final Timer timer;
+
     private final int port;
-    
     private final String host;
 
     private volatile boolean reconnect = true;
     private int attempts;
-    
-    
-    public ConnectionWatchdog(Bootstrap bootstrap, Timer timer, int port,String host, boolean reconnect) {
+
+    public ConnectionWatchdog(Bootstrap bootstrap, Timer timer, int port, String host, boolean reconnect) {
         this.bootstrap = bootstrap;
         this.timer = timer;
         this.port = port;
         this.host = host;
         this.reconnect = reconnect;
     }
-    
+
     /**
      * channel链路每次active的时候，将其连接的次数重新☞ 0
      */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        
         System.out.println("当前链路已经激活了，重连尝试次数重新置为0");
-        
         attempts = 0;
         ctx.fireChannelActive();
     }
-    
+
+//    public ChannelHandler[] handlers() {
+//        return new ChannelHandler[0];
+//    }
+
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
         System.out.println("链接关闭");
-        if(reconnect){
+        if (reconnect) {
             System.out.println("链接关闭，将进行重连");
             if (attempts < 12) {
                 attempts++;
@@ -67,10 +61,10 @@ public abstract class ConnectionWatchdog extends ChannelInboundHandlerAdapter im
         }
         ctx.fireChannelInactive();
     }
-    
+
 
     public void run(Timeout timeout) throws Exception {
-        
+
         ChannelFuture future;
         //bootstrap已经初始化好了，只需要将handler填入就可以了
         synchronized (bootstrap) {
@@ -78,11 +72,11 @@ public abstract class ConnectionWatchdog extends ChannelInboundHandlerAdapter im
 
                 @Override
                 protected void initChannel(Channel ch) throws Exception {
-                    
+
                     ch.pipeline().addLast(handlers());
                 }
             });
-            future = bootstrap.connect(host,port);
+            future = bootstrap.connect(host, port);
         }
         //future对象
         future.addListener(new ChannelFutureListener() {
@@ -94,12 +88,12 @@ public abstract class ConnectionWatchdog extends ChannelInboundHandlerAdapter im
                 if (!succeed) {
                     System.out.println("重连失败");
                     f.channel().pipeline().fireChannelInactive();
-                }else{
+                } else {
                     System.out.println("重连成功");
                 }
             }
         });
-        
+
     }
 
 }
